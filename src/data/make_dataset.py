77c8d0252b0aaba5fbf24412d2221b18f2123486
netr212 <netra200021kcbdr@gmail.com>
2024-11-01 19:37:59 +0000

    Files added due to removal of data folder before dvc tracking

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Readfing data from CSV.
+# Reading data from CSV.
 def read_csv(file_path):
     return pd.read_csv(file_path)
